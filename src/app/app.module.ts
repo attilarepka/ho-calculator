2eac0a36e5f9ce72884799209ab509088b347cff
Attila Repka <repka.attila@yandex.com>
2022-12-20 13:39:08 +0100

    Fix console error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppComponent} from './app.component';
 import {
@@ -9,7 +10,7 @@ import {MaterialModule} from './material-module';
 
 @NgModule({
     declarations : [ AppComponent, MatCalendarWrapperComponent ],
-    imports : [ BrowserModule, MaterialModule ],
+    imports : [ BrowserModule, MaterialModule, BrowserAnimationsModule ],
     providers : [],
     bootstrap : [ AppComponent ]
 })
