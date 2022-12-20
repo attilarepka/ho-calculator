2044c528e062cd119b05766de7418768c0d3e162
Attila Repka <repka.attila@yandex.com>
2022-12-20 09:56:11 +0100

    Update calendar

--- src/app/components/mat-calendar-wrapper/mat-calendar-wrapper.component.ts
+++ src/app/components/mat-calendar-wrapper/mat-calendar-wrapper.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-mat-calendar-wrapper',
-  templateUrl: './mat-calendar-wrapper.component.html',
-  styleUrls: ['./mat-calendar-wrapper.component.css']
+    selector : 'app-mat-calendar-wrapper',
+    templateUrl : './mat-calendar-wrapper.component.html',
+    styleUrls : [ './mat-calendar-wrapper.component.css' ],
 })
 export class MatCalendarWrapperComponent {
+    daysSelected: any[] = [];
+    event: any;
+    calendarStartAt: Date;
 
+    constructor() { this.calendarStartAt = new Date(); }
+
+    ngOnInit() { this.calendarStartAt = new Date('2023-01-01'); }
+
+    isSelected = (event: any) => {
+        const date = event.getFullYear() + '-' +
+                     ('00' + (event.getMonth() + 1)).slice(-2) + '-' +
+                     ('00' + event.getDate()).slice(-2);
+        return this.daysSelected.find((x) => x == date) ? 'selected' : null;
+    };
+
+    select(event: any, calendar: any) {
+        const date = event.getFullYear() + '-' +
+                     ('00' + (event.getMonth() + 1)).slice(-2) + '-' +
+                     ('00' + event.getDate()).slice(-2);
+        const index = this.daysSelected.findIndex((x) => x == date);
+        if (index < 0)
+            this.daysSelected.push(date);
+        else
+            this.daysSelected.splice(index, 1);
+
+        calendar.updateTodaysDate();
+    }
+
+    weekendsDatesFilter = (d: Date): boolean => {
+        const day = d.getDay();
+        const SATURDAY = 0;
+        const SUNDAY = 6;
+
+        return day !== SATURDAY && day !== SUNDAY;
+    };
 }
