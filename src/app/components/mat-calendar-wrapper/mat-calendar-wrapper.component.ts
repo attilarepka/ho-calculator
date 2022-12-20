705a1263da81868848d0bb31430e02044e8d2dd2
Attila Repka <repka.attila@yandex.com>
2022-12-20 13:08:28 +0100

    Add holiday filter

--- src/app/components/mat-calendar-wrapper/mat-calendar-wrapper.component.ts
+++ src/app/components/mat-calendar-wrapper/mat-calendar-wrapper.component.ts
@@ -4,7 +4,7 @@ import {
     Input,
     OnChanges,
     SimpleChanges,
-    ViewEncapsulation
+    ViewEncapsulation,
 } from '@angular/core';
 
 @Component({
@@ -12,7 +12,7 @@ import {
     templateUrl : './mat-calendar-wrapper.component.html',
     styleUrls : [ './mat-calendar-wrapper.component.css' ],
     encapsulation : ViewEncapsulation.None,
-    changeDetection : ChangeDetectionStrategy.OnPush
+    changeDetection : ChangeDetectionStrategy.OnPush,
 })
 export class MatCalendarWrapperComponent implements OnChanges {
     @Input() startMonth: number;
@@ -23,7 +23,7 @@ export class MatCalendarWrapperComponent implements OnChanges {
     constructor() {}
 
     ngOnChanges(changes: SimpleChanges) {
-        const date = new Date();
+        const date = new Date('2023');
         date.setMonth(this.startMonth);
         this.calendarStartAt = date;
     }
@@ -33,7 +33,7 @@ export class MatCalendarWrapperComponent implements OnChanges {
                      ('00' + (event.getMonth() + 1)).slice(-2) + '-' +
                      ('00' + event.getDate()).slice(-2);
         return this.daysSelected.find((x) => x == date) ? 'selected'
-                                                        : null as any;
+                                                        : (null as any);
     };
 
     select(event: any, calendar: any) {
@@ -49,11 +49,23 @@ export class MatCalendarWrapperComponent implements OnChanges {
         calendar.updateTodaysDate();
     }
 
+    holidayFilter = (now: Date):
+        boolean => {
+            // TODO:
+            // make API call
+            // filter to date
+            const holiday =
+                Array(new Date('2023-08-16'), new Date('2023-08-23'));
+            return !holiday.some((e) => now.getFullYear() === e.getFullYear() &&
+                                        now.getMonth() === e.getMonth() &&
+                                        now.getDate() === e.getDate());
+        }
+
     weekendsDatesFilter = (d: Date): boolean => {
         const day = d.getDay();
         const SATURDAY = 0;
         const SUNDAY = 6;
 
-        return day !== SATURDAY && day !== SUNDAY;
+        return day !== SATURDAY && day !== SUNDAY && this.holidayFilter(d);
     };
 }
