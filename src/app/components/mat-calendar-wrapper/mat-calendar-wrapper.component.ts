ef9a90b0b22e98dc261d93edd2a9e29fe7547d6c
Attila Repka <repka.attila@yandex.com>
2022-12-20 11:18:16 +0100

    Update encapsulation

--- src/app/components/mat-calendar-wrapper/mat-calendar-wrapper.component.ts
+++ src/app/components/mat-calendar-wrapper/mat-calendar-wrapper.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
     selector : 'app-mat-calendar-wrapper',
     templateUrl : './mat-calendar-wrapper.component.html',
     styleUrls : [ './mat-calendar-wrapper.component.css' ],
+    encapsulation : ViewEncapsulation.None
 })
 export class MatCalendarWrapperComponent implements OnInit {
     daysSelected: any[] = [];
