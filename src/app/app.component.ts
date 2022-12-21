fca2aa92345ce312ef247f2cb66586c68dc92b60
Attila Repka <repka.attila@yandex.com>
2022-12-21 08:36:14 +0100

    Continue saving WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,34 +5,44 @@ import {
     FileServiceComponent
 } from './services/file-service/file-service.component';
 
+export interface Payload {
+    homeOfficeLimit: number;
+    annualLeaveLimit: number;
+    daysMap: Map<string, string>;
+    currentYear: number;
+}
+
 @Component({
     selector : 'app-root',
     templateUrl : './app.component.html',
     styleUrls : [ './app.component.css' ],
 })
 export class AppComponent {
-    homeOfficeLimit: number = 150;
-    annualLeaveLimit: number;
-    daysMap: Map<string, string> = new Map<string, string>();
-    currentYear: number;
+    payload: Payload;
     selectionType: string = "homeoffice";
     title = 'ho-calc';
     fileService: any;
 
     constructor() {
         this.fileService = new FileServiceComponent(new FileSaverService);
+        this.payload = {
+            currentYear : 2023,
+            homeOfficeLimit : 150,
+            daysMap : new Map<string, string>,
+            annualLeaveLimit : 0
+        }
     }
 
     getNotification(event: any) {
         const key = event.key;
-        if (this.daysMap.get(key) == this.selectionType)
-            this.daysMap.delete(key);
+        if (this.payload.daysMap.get(key) == this.selectionType)
+            this.payload.daysMap.delete(key);
         else
-            this.daysMap.set(key, this.selectionType);
+            this.payload.daysMap.set(key, this.selectionType);
     }
 
     generatePayload() {
-        const payload = "some payload";
+        const payload = JSON.stringify(this.payload);
         this.fileService.onSave(payload);
     }
 
