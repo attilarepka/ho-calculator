c4421be7bff120eea6aaf82eddcf6738878ed00b
Attila Repka <repka.attila@yandex.com>
2022-12-26 12:14:04 +0100

    Refactor codebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,11 @@ import {
     ViewChild,
     ViewChildren
 } from '@angular/core';
-import {FileSaverService} from 'ngx-filesaver';
 
 import {
     MatCalendarWrapperComponent
 } from './components/mat-calendar-wrapper/mat-calendar-wrapper.component';
-import {
-    FileServeService
-} from './services/file-serve.service'
-
-interface Payload {
-    homeOfficeLimit: number;
-    annualLeaveLimit: number;
-    daysMap: Map<string, string>;
-    currentYear: number;
-}
+import {AppService} from './services/app.service';
 
 @Component({
     selector : 'app-root',
@@ -29,122 +19,38 @@ interface Payload {
     styleUrls : [ './app.component.css' ],
 })
 export class AppComponent implements AfterViewInit {
-    payload: Payload;
-    remainingHomeOffice: number;
-    remainingAnnualLeave: number;
-    selectionType: string = "homeoffice";
-    title = 'ho-calculator';
-    fileService: any;
-    selectedFile: File;
+    title = 'ho-calculator'; // can be removed
+    appService: any;
 
     @ViewChildren("calendarChild")
     calendarChild: QueryList<MatCalendarWrapperComponent>;
 
     @ViewChild('fileInput') fileInput: ElementRef;
 
-    constructor() {
-        this.fileService = new FileServeService(new FileSaverService);
-        this.payload = {
-            currentYear : 2023,
-            homeOfficeLimit : 150,
-            daysMap : new Map<string, string>,
-            annualLeaveLimit : 0
-        };
-        this.remainingHomeOffice = this.payload.homeOfficeLimit;
-        this.remainingAnnualLeave = this.payload.annualLeaveLimit;
-    }
+    constructor() { this.appService = new AppService(); }
 
     ngAfterViewInit() {}
 
-    onHomeOfficeLimitChange = ():
+    reset = ():
         void => {
-            this.payload.homeOfficeLimit =
-                Math.min(Math.max(this.payload.homeOfficeLimit, 0), 365);
-            this.remainingHomeOffice = this.payload.homeOfficeLimit;
-        }
-
-    onAnnualLeaveLimitChange = ():
-        void => {
-            this.payload.annualLeaveLimit =
-                Math.min(Math.max(this.payload.annualLeaveLimit, 0), 365);
-            this.remainingAnnualLeave = this.payload.annualLeaveLimit;
-        }
-
-    getNotification = (event: any):
-        void => {
-            const key = event.key;
-            if (this.payload.daysMap.get(key) == this.selectionType)
-                this.payload.daysMap.delete(key);
-            else
-                this.payload.daysMap.set(key, this.selectionType);
-
-            this.updateRemainingDays();
-        }
-
-    updateRemainingDays = ():
-        void => {
-            let usedHomeOffice = 0;
-            let usedAnnualLeave = 0;
-            this.payload.daysMap.forEach((value) => {
-                if (value === "homeoffice")
-                    usedHomeOffice++;
-                else
-                    ++usedAnnualLeave
-            });
-            this.remainingHomeOffice =
-                this.payload.homeOfficeLimit - usedHomeOffice;
-            this.remainingAnnualLeave =
-                this.payload.annualLeaveLimit - usedAnnualLeave;
-        }
-
-    resetPayload = ():
-        void => {
-            this.payload.daysMap.clear();
-            this.payload.annualLeaveLimit = 0;
-            this.payload.homeOfficeLimit = 150;
-            this.remainingHomeOffice = this.payload.homeOfficeLimit;
-            this.remainingAnnualLeave = this.payload.annualLeaveLimit;
-
+            this.appService.reset();
             this.notifyChildren();
         }
 
-    savePayload = ():
-        void => {
-            const payloadJSON = JSON.parse(JSON.stringify(this.payload));
-            const array = Array.from(this.payload.daysMap,
-                                     ([ date, type ]) => ({date, type}));
-            payloadJSON.daysMap = array;
-            this.fileService.onSave(JSON.stringify(payloadJSON));
-        }
+    notifyChildren = (): void => {
+        this.calendarChild.forEach(
+            (child) => child.updateDaysMap(this.appService.getDaysMap()));
+    };
 
-    notifyChildren = ():
-        void => {
-            this.calendarChild.forEach(
-                (child) => child.updateDaysMap(this.payload.daysMap));
-        }
+    loadPayload = async(payload: any): Promise<void> => {
+        this.reset();
 
-    loadPayload = (payload: any):
-        void => {
-            this.resetPayload();
+        await this.appService.load(payload);
 
-            this.selectedFile = payload.target.files[0];
-            const fileReader = new FileReader();
-            fileReader.readAsText(this.selectedFile, "UTF-8");
-            fileReader.onload = () => {
-                const input = JSON.parse(fileReader.result as string);
-                this.payload.currentYear = input.currentYear;
-                this.payload.annualLeaveLimit = input.annualLeaveLimit;
-                this.payload.homeOfficeLimit = input.homeOfficeLimit;
-                input.daysMap.forEach((key: any) => {this.payload.daysMap.set(
-                                          key.date, key.type)});
+        this.notifyChildren();
 
-                this.updateRemainingDays();
-                this.notifyChildren();
-            };
-            fileReader.onerror = (error) => { console.log(error); };
-
-            this.fileInput.nativeElement.value = null;
-        }
+        this.fileInput.nativeElement.value = null;
+    };
 
     @HostListener('contextmenu', [ '$event' ])
     onRightClick(event: {preventDefault: () => void}) {
---
+++ src/app/services/app.service.ts
@@ -0,0 +1,136 @@
+import {Injectable} from '@angular/core';
+import {FileSaverService} from 'ngx-filesaver';
+
+import {FileServeService} from './file-serve.service';
+
+interface Payload {
+    homeOfficeLimit: number;
+    annualLeaveLimit: number;
+    daysMap: Map<string, string>;
+    currentYear: number;
+}
+
+@Injectable({providedIn : 'root'})
+export class AppService {
+    payload: Payload;
+    remainingHomeOffice: number;
+    remainingAnnualLeave: number;
+    selectionType: string = 'homeoffice';
+    fileService: any;
+    selectedFile: File; // can be moved to fileService
+
+    constructor() {
+        this.fileService = new FileServeService(new FileSaverService());
+
+        this.payload = {
+            currentYear : 2023,
+            homeOfficeLimit : 150,
+            daysMap : new Map<string, string>(),
+            annualLeaveLimit : 0,
+        };
+        this.remainingHomeOffice = this.payload.homeOfficeLimit;
+        this.remainingAnnualLeave = this.payload.annualLeaveLimit;
+    }
+
+    setHomeOfficeLimit = (limit: number): void => {
+        this.payload.homeOfficeLimit = Math.min(Math.max(limit, 0), 365);
+        this.remainingHomeOffice = this.payload.homeOfficeLimit;
+    };
+
+    getHomeOfficeLimit = (): number => { return this.payload.homeOfficeLimit; };
+
+    isHomeOfficeAllowed =
+        (): boolean => { return this.remainingHomeOffice > 0; };
+
+    isAnnualLeaveAllowed =
+        (): boolean => { return this.remainingAnnualLeave > 0; };
+
+    hasDaySelection = (): boolean => { return this.payload.daysMap.size > 0; };
+
+    setAnnualLeaveLimit = (limit: number): void => {
+        this.payload.annualLeaveLimit = Math.min(Math.max(limit, 0), 365);
+        this.remainingAnnualLeave = this.payload.annualLeaveLimit;
+    };
+
+    getAnnualLeaveLimit =
+        (): number => { return this.payload.annualLeaveLimit; };
+
+    getRemainingHomeOffice = (): number => { return this.remainingHomeOffice; };
+
+    getRemainingAnnualLeave =
+        (): number => { return this.remainingAnnualLeave; };
+
+    getDaysMap = (): Map<string, string> => { return this.payload.daysMap; };
+
+    setSelectionType =
+        (selection: string): void => { this.selectionType = selection; };
+
+    getSelectionType = (): string => { return this.selectionType; };
+
+    reset = (): void => {
+        this.payload.daysMap.clear();
+        this.payload.annualLeaveLimit = 0;
+        this.payload.homeOfficeLimit = 150;
+        this.remainingHomeOffice = this.payload.homeOfficeLimit;
+        this.remainingAnnualLeave = this.payload.annualLeaveLimit;
+    };
+
+    save = (): void => {
+        const payloadJSON = JSON.parse(JSON.stringify(this.payload));
+        const array = Array.from(this.payload.daysMap, ([ date, type ]) => ({
+                                                           date,
+                                                           type,
+                                                       }));
+        payloadJSON.daysMap = array;
+        this.fileService.onSave(JSON.stringify(payloadJSON));
+    };
+
+    load = async(payload: any): Promise<void> => {
+        this.selectedFile = payload.target.files[0];
+        const fileReader = new FileReader();
+        fileReader.readAsText(this.selectedFile, 'UTF-8');
+        return new Promise((resolve, reject) => {
+            fileReader.onload = async () => {
+                const input = JSON.parse(fileReader.result as string);
+                this.payload.currentYear = input.currentYear;
+                this.payload.annualLeaveLimit = input.annualLeaveLimit;
+                this.payload.homeOfficeLimit = input.homeOfficeLimit;
+                input.daysMap.forEach((key: any) => {
+                    this.payload.daysMap.set(key.date, key.type);
+                });
+
+                this.updateRemainingDays();
+                resolve();
+            };
+            fileReader.onerror = (error) => {
+                console.log(error);
+                return reject(error);
+            };
+        });
+    };
+
+    updateRemainingDays = (): void => {
+        let usedHomeOffice = 0;
+        let usedAnnualLeave = 0;
+        this.payload.daysMap.forEach((value) => {
+            if (value === "homeoffice")
+                usedHomeOffice++;
+            else
+                ++usedAnnualLeave
+        });
+        this.remainingHomeOffice =
+            this.payload.homeOfficeLimit - usedHomeOffice;
+        this.remainingAnnualLeave =
+            this.payload.annualLeaveLimit - usedAnnualLeave;
+    };
+
+    setSelectedDay = (event: any): void => {
+        const key = event.key;
+        if (this.payload.daysMap.get(key) == this.selectionType)
+            this.payload.daysMap.delete(key);
+        else
+            this.payload.daysMap.set(key, this.selectionType);
+
+        this.updateRemainingDays();
+    };
+}
